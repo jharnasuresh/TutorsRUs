83af861e934eab6cf1c6a8337b5a11d926447046
evanstang <89542370+evanstang@users.noreply.github.com>
2023-02-17 15:45:41 -0500

    login and register and css

---
+++ home/src/Login.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react"
+export const Login = (props) => {
+    const [user, setUsername] = useState('');
+    const [pass, setPassword] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user);
+    }
+    return (
+        <div className = "auth-form-container">
+        <form classname="login-form" onSubmit={handleSubmit}>
+            <label htmlFor="username:">username</label>
+            <input value={user} onChange={(e) => setUsername(e.target.value)}type="username" placeholder="enter your username" id="username" name="username"/>
+            <br></br>
+            <label for="password:">password</label>
+            <input value={pass} onChange={(e) => setPassword(e.target.value)}type="password" placeholder="enter your password" id="password" name="password"/>
+            <br></br>
+            <button type = "submit">Log In</button>
+        </form>
+        <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here. </button>
+     </div>
+    )
+}
---
+++ home/src/Register.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react"
+export const Register = (props) => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [user, setUsername] = useState('');
+    const [pass, setPassword] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user);
+    }
+    return (
+        <div className = "auth-form-container">
+        <form classname="register-form" onSubmit={handleSubmit}>
+            <label htmlFor="firstName:">First Name</label>
+            <input value={firstName} onChange={(e) => setFirstName(e.target.value)}type="firstName" placeholder="enter your first name" id="firstName" name="firstName"/>
+            <br></br>
+            <label htmlFor="lastName:">Last Name</label>
+            <input value={lastName} onChange={(e) => setLastName(e.target.value)}type="lastName" placeholder="enter your last name" id="lastName" name="lastName"/>
+            <br></br>
+            <label htmlFor="username:">user</label>
+            <input value={user} onChange={(e) => setUsername(e.target.value)}type="username" placeholder="enter your username" id="username" name="username"/>
+            <br></br>
+            <label htmlFor="password:">password</label>
+            <input value={pass} onChange={(e) => setPassword(e.target.value)}type="password" placeholder="enter your password" id="password" name="password"/>
+            <br></br>
+            <button type="submit"> Register </button>
+        </form>
+        <button onClick={() => props.onFormSwitch('login')}> Already have an account? Login here. </button>
+    </div>
+    )
+    
+}
