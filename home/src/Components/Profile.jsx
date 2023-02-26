f9aedbdd73bc28d44aeed1ce0ac20ec14f37e4f4
Malia Marquez <mkmarque@purdue.edu>
2023-02-25 23:16:19 -0500

    fix mailto

--- home/src/Components/Profile.jsx
+++ home/src/Components/Profile.jsx
@@ -28,14 +28,29 @@ export const Profile = () => {
 
     return;
 }
-
 const location = useLocation();
 console.log("l " + location.state.u)
+async function getInfo(location) {
+
+
+const requestData = JSON.stringify({ "username": location.state.u});
+const headers = { "content-type": "application/json" };
+const response = await fetch('http://localhost:3001/info', { method: 'POST', body: requestData, headers: headers })
+var r = await response.json();
+return r;
+
+}
+
+var r = getInfo();
+console.log("name = " + r['fname'])
+
+
+
     return (
       <div className="App Profile">
-        <Header fname={location.state.fname} lname={location.state.lname}/>
+        <Header fname={location.state.fname} lname={r['fname']}/>
         <hr />
-        <About user={location.state.u} email={location.state.email}/>
+        <About user={location.state.u} email={location.state.email} mail={"mailto:" + location.state.email}/>
         <br></br>
           <a href="/Profile">
             <button className="submit" > {location.state.u} </button>
