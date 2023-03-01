1c23f6181890324bfcf84106597ab6940f4d3d75
evanstang <89542370+evanstang@users.noreply.github.com>
2023-03-01 16:03:56 -0500

    Edit Course UI done

--- home/src/Components/Settings.jsx
+++ home/src/Components/Settings.jsx
@@ -110,7 +110,7 @@ export const Settings = ({ GlobalState }) => {
                 <label htmlFor="email">Email: </label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email Address" id="email" name="email" />
                 <br></br>
-                <button type="submit">Edit Courses</button>
+                <button type="submit" onClick={() => navigate('/EditCourse', {state: {u: currUser}})}>Edit Courses</button>
                 <br></br>
                 <button type="submit" className="setting-sub" onSubmit={handleSubmit}>Submit Changes</button>
             </form>
@@ -124,34 +124,6 @@ export const Settings = ({ GlobalState }) => {
             </div>
             <button className='submit' onClick={handleDeactivate}>{aButton} Account</button>
         </div>
-        /*
-        <div className="App Profile">
-            <h2> Edit Profile </h2>
-            <form className="settings-name" onSubmit={handleSubmit}>
-                <label htmlFor="name">Name: </label>
-                <input value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder="Enter Your Full Name" id="name" name="name" />
-            </form>
-
-            <form className="settings-username" onSubmit={handleSubmit}>
-                <label htmlFor="username">Username: </label>
-                <input value={username} onChange={(e) => setUsername(e.target.value)} type="username" placeholder="Username" id="username" name="username" />
-            </form>
-
-            <form className="settings-password" onSubmit={handleSubmit}>
-                <label htmlFor="password">Password: </label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
-            </form>
-
-            <form className="settings-email" onSubmit={handleSubmit}>
-                <label htmlFor="email">Email: </label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email Address" id="email" name="email" />
-            </form>
-            <button type="submit" className="setting-sub">Submit Changes</button>
-            <button type="submit" onClick={backToProfile}>Back to profile</button>
-
-            <button type="submit" className="deactivate-btn">Deactivate Account</button>
-        </div>
-        */
     );
 }
 
