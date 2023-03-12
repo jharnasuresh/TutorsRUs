4ee02a80976860e9957fcf917bea599a033c6e9f
evanstang <89542370+evanstang@users.noreply.github.com>
2023-03-12 16:16:31 -0400

    add UI

--- home/src/Components/UploadProfile.jsx
+++ home/src/Components/UploadProfile.jsx
@@ -1,6 +1,9 @@
 
 import Avatar from "@mui/material/Avatar";
 import { useState } from "react";
+import './Main.css'
+
+import { Route, useHref, useNavigate, useLocation, Link } from "react-router-dom";
 /*import { storage } from "./firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 */
@@ -9,6 +12,7 @@ export const UploadProfile = ({GlobalState}) => {
 const { currUser, setCurrUser } = GlobalState;
   const [image, setImage] = useState(null);
   const [url, setUrl] = useState(null);
+  const location = useLocation();
 
   const handleImageChange = (e) => {
     if (e.target.files[0]) {
@@ -40,6 +44,9 @@ const { currUser, setCurrUser } = GlobalState;
       <Avatar src={url} sx={{ width: 150, height: 150 }} />
       <input type="file" onChange={handleImageChange} />
       <button onClick={handleSubmit}>Submit</button>
+      <button className='submit'>
+        <Link to="/Settings" state={{ user: location.state.u, active: location.state.active, taking: location.state.taking, taken: location.state.taken, tutor: location.state.tutor }}>Back</Link>
+    </button>
     </div>
   );
 }
