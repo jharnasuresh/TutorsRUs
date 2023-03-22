8b51b389da7961e0f4ba676149b933ae19a8ec83
Malia Marquez <mkmarque@purdue.edu>
2023-03-22 19:49:27 -0400

    start radio buttons

--- home/src/Components/Search.jsx
+++ home/src/Components/Search.jsx
@@ -5,9 +5,9 @@ import { useLocation, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 
-function Search({GlobalState, placeholder, data}) {
+function Search({ GlobalState, placeholder, data }) {
     const [obj, setObj] = useState({});
-    const [sort, setSort] = useState({sort: "rating", order: "desc"});
+    const [sort, setSort] = useState({ sort: "rating", order: "desc" });
     const [filterGenre, setFilterGenre] = useState([])
     const [page, setPage] = useState(1);
     const [open, setOpen] = useState(false);
@@ -15,16 +15,17 @@ function Search({GlobalState, placeholder, data}) {
     const [search, setSearch] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
+    const [searchBy, setSearchBy] = useState('Courses')
 
     useEffect(() => {
         const getAllMovies = async () => {
             try {
                 const url = `${"hello"}?page=${page}&sort=${sort.sort},${sort.order}
                     &genre=${filterGenre.toString()}&search=${search}`
-                const {data} = await axios.get(url);
+                const { data } = await axios.get(url);
                 setObj(data);
                 console.log(data);
-            } catch(err) {
+            } catch (err) {
                 console.log(err);
             }
         };
@@ -51,7 +52,9 @@ function Search({GlobalState, placeholder, data}) {
 
     const handleSubmit = (e) => {
         console.log("searching for " + search)
-        
+        setSearchBy(document.getElementsByName("search-by").value)
+        console.log("search by: " + searchBy)
+
 
         if (search.includes(",")) {
             // search for multiple courses
@@ -59,69 +62,71 @@ function Search({GlobalState, placeholder, data}) {
             const headers = { "content-type": "application/json" };
             const requestData = JSON.stringify({ "course": search });
             fetch('http://localhost:3001/searchmultiplecourses', { method: 'POST', body: requestData, headers: headers })
-            .then((res) => res.json())
-            .then((res) => {
-                console.log(res)
-                if (res === "none") {
-                    // no tutors
-                    console.log("none here")
-                    setNone(true)
-                }
-                else {
-                    // list tutors
-                    setNone(false);
-                    // TO BE FIXED WITH MORE DATA
-                    let i = 0;
-                    let u = "";
-                    while (i < res.length) {
-                        console.log(res[i]);
-                        u += res[i] + ", ";
-                        i++;
+                .then((res) => res.json())
+                .then((res) => {
+                    console.log(res)
+                    if (res === "none") {
+                        // no tutors
+                        console.log("none here")
+                        setNone(true)
                     }
-                    setUsers(u)
-                    
-                    
-                }
+                    else {
+                        // list tutors
+                        setNone(false);
+                        // TO BE FIXED WITH MORE DATA
+                        let i = 0;
+                        let u = "";
+                        while (i < res.length) {
+                            console.log(res[i]);
+                            u += res[i] + ", ";
+                            i++;
+                        }
+                        setUsers(u)
 
-            })
+
+                    }
+
+                })
         } else {
             const headers = { "content-type": "application/json" };
             const requestData = JSON.stringify({ "course": search });
             fetch('http://localhost:3001/searchcoursetitle', { method: 'POST', body: requestData, headers: headers })
-            .then((res) => res.json())
-            .then((res) => {
-                console.log(res)
-                if (res === "none") {
-                    // no tutors
-                    console.log("none here")
-                    setNone(true)
-                }
-                else {
-                    // list tutors
-                    setNone(false);
-                    // TO BE FIXED WITH MORE DATA
-                    let i = 0;
-                    let u = "";
-                    while (i < res.length) {
-                        console.log(res[i]);
-                        u += res[i] + ", ";
-                        i++;
+                .then((res) => res.json())
+                .then((res) => {
+                    console.log(res)
+                    if (res === "none") {
+                        // no tutors
+                        console.log("none here")
+                        setNone(true)
                     }
-                    setUsers(u)
-                    
-                    
-                }
+                    else {
+                        // list tutors
+                        setNone(false);
+                        // TO BE FIXED WITH MORE DATA
+                        let i = 0;
+                        let u = "";
+                        while (i < res.length) {
+                            console.log(res[i]);
+                            u += res[i] + ", ";
+                            i++;
+                        }
+                        setUsers(u)
 
-            })
+
+                    }
+
+                })
         }
 
-        
+
 
 
     }
 
     return (
+
         <div className="App search">
+            {/*
             <div className="wrapper">
                 <div className="container">
                     <div className="body">
@@ -130,16 +135,32 @@ function Search({GlobalState, placeholder, data}) {
                     </div>
                 </div>
             </div>
+    */}
+    <form>
+    <div className="searchby">
+                <label for="courses" style={{padding: '6px 20px'}}>Courses</label>
+                <input type="radio" id="courses" name="search-by" value="Courses" style={{height: '20px', width: '20px', paddingInline: '20px'}}/>
+                <label for="tutor" style={{padding: '6px 20px'}}>Tutor</label>
+                <input type="radio" id="tutor" name="search-by" value="Tutor" style={{height: '20px', width: '20px', padding: '20px'}}/>
+                <label for="professor" style={{padding: '6px 20px'}}>Professor</label>
+                <input type="radio" id="Professor" name="search-by" value="Professor" style={{height: '20px', width: '20px', padding: '20px'}}/>
+                
+            </div>
+    </form>
             
+
+
+
+
             <div className="searchInputs">
 
-                <input type="text" placeholder="Search..." onChange={e => setSearch(e.target.value)}/>
+                <input type="text" placeholder="Search..." onChange={e => setSearch(e.target.value)} />
                 <div className="searchIcon">
                     <button type="link-btn" onClick={handleSubmit}><i class="fa-solid fa-magnifying-glass"></i></button>
                 </div>
             </div>
             {
-                none ? <h1 style={{color: 'red'}}>No available tutors</h1> : <p>{users}</p>
+                none ? <h1 style={{ color: 'red' }}>No available tutors</h1> : <p>{users}</p>
             }
         </div>
     )
