5df7234af60fc4e128c89d5287397fc9f6fa9ab9
evanstang <89542370+evanstang@users.noreply.github.com>
2023-04-16 21:14:10 -0400

    alignment for profile

--- home/src/Components/About.jsx
+++ home/src/Components/About.jsx
@@ -16,26 +16,26 @@ class About extends Component {
                     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Bowlby+One" />
                      <h2>{this.props.user}</h2>
                      </div>
-                    <h2 style={{display: 'inline', paddingRight: '50px'}}>Primary Language:</h2> 
+                    <h2 style={{display: 'inline', paddingRight: '50px', marginLeft: '-190px'}}>Primary Language:</h2> 
                     <p style={{display: 'inline'}}>{ this.props.lang === "" ? "English" : this.props.lang} </p>
                     <br></br>
                     <br></br>
-                    <h2 style={{display: 'inline', paddingRight: '50px'}}>Email:</h2> 
+                    <h2 style={{display: 'inline', paddingRight: '50px', marginLeft: '-150px'}}>Email:</h2> 
                     {
                         (this.props.yours) ? <p style={{display: 'inline',paddingRight: '50px'}}>{this.props.email}</p> : <a style={{display: 'inline', paddingRight: '50px'}} href={this.props.mail}>{this.props.email}</a>
                     }
                     <br></br>
                     <br></br>
-                    <h2 style={{display: 'inline'}}>Rating:</h2>
+                    <h2 style={{display: 'inline', marginLeft: '-445px'}}>Rating:</h2>
                     {
                         this.props.follows ? <p style={{display: 'inline', paddingRight: '50px'}}>{this.props.studentRating}/5.0 </p> : <p></p>
                     }
                     <br></br>
                     {
-                        (this.props.tutor) ? <><h2 style={{display: 'inline', paddingRight: '50px'}}>Price:</h2><p style={{display: 'inline'}}>${this.props.price}/hour</p> 
+                        (this.props.tutor) ? <><h2 style={{display: 'inline', paddingRight: '50px', marginLeft: '-360px'}}>Price:</h2><p style={{display: 'inline'}}>${this.props.price}/hour</p> 
                         <br></br>
                         <br></br>
-                        <h2 style={{display: 'inline', paddingRight: '50px'}}>Tutor Rating:</h2><p style={{display: 'inline'}}>{this.props.tutorRating}/5.0</p>
+                        <h2 style={{display: 'inline', paddingRight: '50px', marginLeft: '-285px'}}>Tutor Rating:</h2><p style={{display: 'inline'}}>{this.props.tutorRating}/5.0</p>
                         </>
                         : <span></span>
                     }
