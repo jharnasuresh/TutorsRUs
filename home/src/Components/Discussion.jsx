69da3a3a5baada333470b7b5f94a6651be26bd07
shreyasuri11 <83329054+shreyasuri11@users.noreply.github.com>
2023-04-20 16:18:09 -0400

    video UI etc

--- home/src/Components/Discussion.jsx
+++ home/src/Components/Discussion.jsx
@@ -5,6 +5,7 @@ import CreateDiscussion from './CreateDiscussion'
 import ReportPost from './ReportPost';
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import { blue } from 'tailwindcss/colors';
+import { Directions } from '@mui/icons-material';
 {/*class Discussion extends Component {
     render() {
         return (
@@ -109,26 +110,30 @@ export const Discussion = ({GlobalState}) => {
     return (
         
         <div className = "App">
-             <div style={{padding: "20px", fontFamily: "Bowlby One", color: "rgb(96, 44, 145)", marginTop: '-700px'}}>
+             <div style={{padding: "20px", fontFamily: "Bowlby One", color: "rgb(96, 44, 145)", marginTop: '100px'}}>
                         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Bowlby+One" />
                         <h1>Your Discussion Boards!</h1>
             </div>
+            <div clasName = "flex" style={{marginTop:"50px"}}>
             {
     
                 location.state.boards.map((b) => (
                    
-  
-                    <div style={{marginTop: "1000px", display: "inline-block", position: "relative", width: '600px', height: '100px', textAlign: 'center', border: 'solid', backgroundColor: 'white', color: 'gray', borderRadius: '10px', padding: '5px', marginTop: "20px"}}>
+                    
+                    <div style={{ marginTop: "20px", flexDirection: "column", position: "relative", width: '600px', height: '100px', textAlign: 'center', border: 'solid', backgroundColor: 'white', color: 'gray', borderRadius: '10px', padding: '5px'}}>
                         <button className="linked" onClick={(e) => {toBoard(b)}}>{b}</button>
                         <button onClick={(e) => {leaveBoard(b)}}>Leave</button>
+                        <br></br>
                     </div>
+                
                    
                     
                 )
                 )
             }
+            </div>
 
-                <div style={{marginTop: "500px"}} class = "Discussion">
+                <div style={{marginTop: "100px"}} class = "Discussion">
             <button type="submit" onClick={() => navigate('/CreateDiscussion', {state: {u: currUser}})}>Create New Discussion Board</button>        
              
         </div>
