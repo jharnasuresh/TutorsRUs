65bf0288bf65d3566ad4ddfd49a75b4587051eb1
Malia Marquez <mkmarque@purdue.edu>
2023-04-20 20:40:18 -0400

    fix report

--- home/src/Components/ReportPost.jsx
+++ home/src/Components/ReportPost.jsx
@@ -7,21 +7,22 @@ export const ReportPost = ({GlobalState}) => {
     const location = useLocation();
     const [reasonWhy, setreasonWhy] = useState('');
     const [reported, setReported] = useState(false);
-    const { currUser, setCurrUser } = useState(location.state.u);
+    //const { currUser, setCurrUser } = useState(location.state.u);
 
     return (
         <div class="App">
         <div style={{backgroundColor: 'white', marginTop: '-500px', width: '600px', height: '500px', borderRadius: '10px', border: '5px solid rgb(180, 180, 180)', padding: '80px'}} class="reportPost">
-                <form >
+                
                         <h2 style={{padding: '40px'}}div="h2">Report a Post</h2>
                         
                         <label htmlFor="why">Why does this post violate Purdue guidelines?</label>
-                        <input style={{padding: '40px'}} value={reasonWhy} onChange={(e) => {setreasonWhy(e.target.value)}} type="why" placeholder="Enter Your Explanation:" id="why" name="why" />
+                        <input style={{padding: '40px'}} value={reasonWhy} onChange={(e) => setreasonWhy(e.target.value)} type="why" placeholder="Enter Your Explanation:" id="why" name="why" />
+                        
+                        <button type="submit" onClick={(e)=>setReported(true)} >Submit</button>
                         {
                             reported ? <p> You have successfully reported the post </p>:console.log()
                         }
-                        <button type="submit" onClick={(e)=>{setReported(true)}} >Submit</button>
-                </form>
+                
              
         </div>
         </div>
